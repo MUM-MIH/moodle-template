0247fcd996cee9fab51eb5243104347423e63c81
MUM-MIH <83559822+MUM-MIH@users.noreply.github.com>
2021-05-03 13:37:32 +0800

    Create overviewaccordion.js

---
+++ overviewaccordion.js
@@ -0,0 +1,8 @@
+$(function() {
+        $("#accordion1").accordion({
+          header: "h5",
+          collapsible: false,
+          heightStyle: "content",
+          active: 0,
+        });
+      });
