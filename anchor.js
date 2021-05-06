6d6b169fe1790151ddb68b935e6aeaac5cad6c4b
MUM-MIH <83559822+MUM-MIH@users.noreply.github.com>
2021-05-06 21:20:26 +0800

    Update anchor.js
    Added h3 to anchor.

--- anchor.js
+++ anchor.js
@@ -1,4 +1,4 @@
-window.onload = function() {document.querySelectorAll('.single-section h2').forEach($heading => {
+window.onload = function() {document.querySelectorAll('.single-section h2, h3').forEach($heading => {
 
     //create id from heading text
   var id = $heading.getAttribute("id") || $heading.innerText.toLowerCase().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/ +/g, '-');
