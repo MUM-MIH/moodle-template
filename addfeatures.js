0397d312a6854f9900e09a4c605cfa8c387a10ac
MUM-MIH <83559822+MUM-MIH@users.noreply.github.com>
2021-05-12 11:46:29 +0800

    Update addfeatures.js
    changes staff resources to design resources so it will hide the correct section.

--- addfeatures.js
+++ addfeatures.js
@@ -13,12 +13,12 @@ window.onload=function(){
 					$('#inst3407551 .action-menu-trigger').css('display','none');
 					$('#inst3407542 .action-menu-trigger').css('display','none');
 					$("#nav-drawer .list-group a").each(function(){
-						if($(this).text().match(/Staff resources/)){
+						if($(this).text().match(/Design Resources/)){
 
 //make a variable of the Moodle section name
 							var moodleSectionName = $('.sectionname').text();
 //check the variable for the text Staff resources. Make sure the section you are on is the staff resources section.
-							if(moodleSectionName.indexOf("Staff resources") > -1){
+							if(moodleSectionName.indexOf("Design Resources") > -1){
 							if(window.location.href.indexOf('section-0') > -1 || window.location.href.indexOf('section=0') > -1) {	
 //If you are on the staff resources section, remove the ability to hide/show the section to students. Ensuring it is not accidentally shown to students.
 							//do nothing						      
@@ -30,7 +30,7 @@ window.onload=function(){
 //Search the left hand nav for the link to the Staff resources section, and make a variable out of that link.
 							var SRSectionLink=$("nav .list-group-item:contains('Staff resources')").attr('href');
 //Create a new button on the end of the nav labelled staff resources.
-							$('.arts-banner-nav div').last().after('<div id="arts-staff-resources"><a href="https://lms.monash.edu/course/view.php?id=97270&amp;section=1"><i class="fa fa-user fa-fw" aria-hidden="true"></i> Staff resources</a></div>');
+							$('.arts-banner-nav div').last().after('<div id="arts-staff-resources"><a href="https://lms.monash.edu/course/view.php?id=97270&amp;section=1"><i class="fa fa-user fa-fw" aria-hidden="true"></i> Design Resources</a></div>');
 //Change the link of the new button to match the link of the section Staff resources resides in.
 							$('#arts-staff-resources a').attr('href', SRSectionLink);
 //Check the section the staff member is on is the overview section. If so, add the template instructions.
