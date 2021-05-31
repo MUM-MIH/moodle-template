dcc2f10e9b83cda736270f082c4bf6f4ba51b993
MUM-MIH <83559822+MUM-MIH@users.noreply.github.com>
2021-05-31 12:54:59 +0800

    Update addfeatures.js
    Tested and now works. Previously maybe blocked by accordion js.

--- addfeatures.js
+++ addfeatures.js
@@ -1,5 +1,8 @@
-/* // directs the function to work once the page has loaded.
+// directs the function to work once the page has loaded.
 window.onload=function(){
+// change all links that have #section- in them which goes to the all sections page, to &section= links that go to the single section instead
+	$('nav a, .bookexit').each(function(){
+        	this.href=this.href.replace('#section-','&section=');
 // opens the atto editor completely on load    
     		$('.editor_atto_toolbar div').removeAttr('hidden');
   		$('.editor_atto_toolbar div').css('display', 'inline-block');
@@ -7,91 +10,5 @@ window.onload=function(){
    		$('.mceToolbarRow2').css('display', 'table');
   		$('.mceToolbarRow3').css('display', 'table');
   	});
-
-// the code below allows for popups
-
-// define expiration duration as 144 hours
-	const expirationDuration = 1000 * 60 * 60 * 144;
-// save the time of the current login to localStorage
-	const savedTime = localStorage.getItem('savedTime');
-// get the time of the current login
-	const currentTime = new Date().getTime();
-// make a constant that refers to when there is no record of a login
-	const notAccepted = savedTime == undefined;
-// make a constant that refers to when the login has a history, and meets the requirements to display the notification again
-	const AcceptedExpired = savedTime != undefined && currentTime - savedTime > expirationDuration;
-// Australian attendance requirements notification
-	$('.sectionname').before('<div class="container"><div class="modal fade" id="attNotification" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Attendance expectations</h4><button type="button" class="close" data-dismiss="modal">&times;</button></div><div class="modal-body"><p>The Arts Faculty has an expectation that you attend all scheduled workshops and tutorials, and participate in all learning activities. All the evidence suggests that student success is greatly impacted by class attendance and participation.</p><p>If you are unable to attend a scheduled workshop or tutorial, please contact your tutor or unit coordinator, and ensure you have strategies in place to catch up on any missed work.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div></div>');
-// Malaysian attendance requirements notification
-	$('.sectionname').before('<div class="container"><div class="modal fade" id="attNotificationMalaysia" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Attendance expectations</h4><button type="button" class="close" data-dismiss="modal">&times;</button></div><div class="modal-body"><p>The Monash Arts Faculty, and within that your School \'SASS\', have an expectation that you attend all scheduled workshops and tutorials, and participate in all learning activities. All the evidence suggests that student success is greatly impacted by class attendance and participation. This is true of online and face-to-face learning.</p><p>If you are unable to attend a scheduled workshop or tutorial, please contact your tutor or unit coordinator, and work with them to ensure you have strategies in place to catch up on any missed work.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div></div>');
-// if the unit is a Malaysia unit, display the Malaysia modal
-	if(headerTitle.indexOf("AMU") >= 1 || headerTitle.indexOf("AMG") >= 1){
-  		if(notAccepted || AcceptedExpired){
-  			$('#attNotificationMalaysia').modal('show');
-  			localStorage.setItem("savedTime", currentTime);
-  		}
-   		else{
-      //Do nothing
-    		}
-	}
-// if the unit is an Australian unit, display the Australian modal
-	else if (headerTitle.indexOf("APG") >= 1 || headerTitle.indexOf("ATS") >= 1){
- 		if(notAccepted || AcceptedExpired){
-  		$('#attNotification').modal('show');
-  		localStorage.setItem("savedTime", currentTime);
- 		}
-    		else{
-      		//Do nothing
-    		}
-	}
-//add material to our FoA category if you are a tutor, lect, non-primary lect, designer or admin
-//check the role level. Tutor and above have access to the recycle bin link. So the material that follows will not display to students.
-//Check the admin block. Check each link in the block.
-	$(".block-region .type_course a").each(function(){
-//Look for the text Recycle bin to check it's a tutor or above.		
-		if($(this).text().match(/Recycle bin/)){
-			//link to how to's in the uni wide banner and nav drawer
-					$( ".header-right" ).prepend('<div class="custom-menus my-auto"><a href="http://bit.ly/artshowtos" target="_blank" class="border border-dark rounded-circle text-dark p-2" role="button" title="Ed Tech how tos"><i class="fa fa-bolt fa-fw" aria-hidden="true"></i></a></div>');
-	    				$( "#nav-drawer .list-group" ).prepend('<a class="list-group-item list-group-item-action" href="http://bit.ly/artshowtos" target="_blank" data-key="coursehome" data-isexpandable="0" data-indent="0" data-showdivider="0" data-type="60" data-nodetype="0" data-collapse="0" data-forceopen="0" data-isactive="0" data-hidden="0" data-preceedwithhr="0"><div class="ml-0"><div class="media"><span class="media-left"><i class="icon fa fa-bolt fa-fw " aria-hidden="true"></i></span><span class="media-body ">Ed Tech how tos</span></div></div></a>');
-					$('.section_action_menu .editing_delete').css('display','none');
-					$('#inst3407551 .action-menu-trigger').css('display','none');
-					$('#inst3407542 .action-menu-trigger').css('display','none');
-					$("#nav-drawer .list-group a").each(function(){
-						if($(this).text().match(/Staff resources/)){
-             
-//make a variable of the Moodle section name
-							var moodleSectionName = $('.sectionname').text();
-//check the variable for the text Staff resources. Make sure the section you are on is the staff resources section.
-							if(moodleSectionName.indexOf("Design resources") > -1){
-							if(window.location.href.indexOf('section-0') > -1 || window.location.href.indexOf('section=0') > -1) {	
-//If you are on the staff resources section, remove the ability to hide/show the section to students. Ensuring it is not accidentally shown to students.
-							//do nothing						      
-							}
-							else{
-							$('.section-actions .editing_showhide').remove();
-							} 
-							}
-//Search the left hand nav for the link to the Staff resources section, and make a variable out of that link.
-							var SRSectionLink=$("nav .list-group-item:contains('Design resources')").attr('href');
-//Create a new button on the end of the nav labelled staff resources.
-							$('.arts-banner-nav div').last().after('<div id="arts-staff-resources"><a href="https://lms.monash.edu/course/view.php?id=97270&amp;section=1"><i class="fa fa-user fa-fw" aria-hidden="true"></i> Staff resources</a></div>');
-//Change the link of the new button to match the link of the section Staff resources resides in.
-							$('#arts-staff-resources a').attr('href', SRSectionLink);
-//Check the section the staff member is on is the overview section. If so, add the template instructions.
-			/*var overviewSection=$("nav .list-group-item:contains('Overview')").attr('href');
-			var currentSection=window.location.href;
-			var callistaUnitCode = document.querySelector('.block_callista a')
-			? document.querySelector('.block_callista a').innerText
-			: null;
-			if (overviewSection == currentSection && headerTitle.indexOf('Winter') > -1){
- 				$('#templateInstructions').after('<div class="mt-3 p-2 border-mu-3 rounded mx-3 mb-2" id="staffInstructions"><h3 class="text-center"><i class="fa fa-fw fa-info-circle"></i> Welcome to your Moodle template</h3><p>These instructions are temporary and will disappear in O week. You cannot duplicate or edit this panel. The panel only appear to staff members, not students. Please click the button below to reveal further information on how to populate your Moodle site using this template.</p><div class="mt-3"><a class="btn btn-lg btn-secondary btn-block w-50 mb-2 mx-auto" data-toggle="collapse" href="#moodleTemplateInstructions" role="button" aria-expanded="false" aria-controls="collapseExample">Show/Hide Moodle template FAQs <i class="fa fa-fw fa-eye"></i></a></div><div class="collapse row w-100" id="moodleTemplateInstructions"><div class="col-lg"><h6 class="font-weight-bold">What is the Arts Moodle template?</h6><p>It is a Moodle site with pre-populated headings to support a clear and consistent organisation of content and learning activities.</p><h6 class="font-weight-bold">Why use a template?</h6><p>The template approach establishes a ‘road map’ for student learning. All learning materials and activities are signposted so that students can easily navigate through a logical learning pathway. The template provides a consistent experience for students across all their units in the Faculty.</p><h6 class="font-weight-bold">Can I see examples of a past unit using this template?</h6><p>Yes. The examples linked below contain links to useful instructional guides and educational research for you to consider in your unit design. We have provided two different ‘learning pathways’, or structures, to assist you in organising your material:</p><ul><li>For an example of a unit using a categories (flexible) structure, <a href="https://lms.monash.edu/course/view.php?id=89913&section=1" target="_blank" title="flexible design structure example">please see here</a> (opens in a new window). All UG units not using a template yet have been provided with this structure.</li><li>For an example of a unit using a pre/in/post-class (sequenced) structure for weeks/topics, <a href="https://lms.monash.edu/course/view.php?id=90111&section=1" target="_blank" title="sequenced design structure example">please see here</a> (opens in a new window). All LLCL and PG units are already using this structure.</li></ul><p>If you wish to change the structure you have been provided with, please edit the headings in your weeks/topics pages accordingly, or contact the Teaching Support or Educational Design teams (see below for links).</p><h6 class="font-weight-bold">How do I use the template?</h6><p>Import your content from a previous Moodle site, or build your Moodle site content from scratch. See these guides for further information (all links open in a new window):</p><ul><li><a href="https://sites.google.com/monash.edu/arts-teaching-and-learning-hq/digital-design/arts-moodle-template-guidelines" target="_blank" title="Checklist for the Arts Moodle Template">Checklist for using the Arts Moodle template</a></li><li><a href="https://lms.monash.edu/course/view.php?id=48399&section=11" target="_blank" title="Ed design how to\'s">Ed design how to\'s</a></li><li><a href="https://sites.google.com/monash.edu/arts-teaching-and-learning-hq" target="_blank" title="Arts T&L HQ">Arts T&L HQ site</a></li></ul><p><strong>Remember:</strong> clicking the lightning bolt icon <i class="fa fa-bolt p-2 border border-dark rounded-circle text-center" style="width:32px;"></i> in the white banner at the top right corner of your Moodle site will take you to our full suite of instructional guides.<h6 class="font-weight-bold">Who do I contact for support?</h6><p>the <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=arts-educational-design@monash.edu&su=Ed%20design%20support%20for%20my%20unit" target="_blank" title="email Ed Design team">Educational Design team</a> (opens in a new window) can advise you on how best to organise your material and structure your learning activities. Your local <a href="https://www.intranet.monash/arts/academic-governance-students/teaching-support/contact-us" target="_blank" title="Teaching Support contact list">Teaching Support Officer</a> (opens in a new window) can assist with your Moodle site set up.</p><h6 class="font-weight-bold">Why are these instructions appearing in my Moodle site?</h6><p>This instruction panel is temporary. <strong>It appears here to staff only</strong> via a script written by the Educational Design team and <strong>will be removed before students gain access to the site</strong>.</p></div></div></div>');
-  			};	
-//Close off IF statement relating to checking the role level of the staff member
-  							};
-						});
-					};
-//Close off function to check for the recycle bin link		
-				});
 //Close window.onload function	
-	}; */
-
+	};
